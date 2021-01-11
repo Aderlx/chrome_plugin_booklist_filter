9777a61aa1aad6900e8c7d18b113419f6ba63622
Aderlx <aderlx@outlook.com>
2021-01-11 09:35:03 +0800

    添加自动翻页功能

--- inject.js
+++ inject.js
@@ -47,6 +47,7 @@ function choose_num(num_status = 2) {
 
 }
 
+// 过滤
 function filter() {
     let res = document.getElementsByClassName("common-card-layout booklist-book-item");
     // 隐藏 书籍状态筛选结果
@@ -69,6 +70,7 @@ function filter() {
 
 }
 
+// 还原过滤操作
 function reload() {
     let res = document.getElementsByClassName("common-card-layout booklist-book-item");
     for (let i = 0; i < res.length; i++) {
@@ -76,7 +78,50 @@ function reload() {
     }
 }
 
+function get_all_page_url(current_url,page){
+    if (current_url.indexOf("page") < 0 ){
+        if (current_url.indexOf("?") < 0 ) {
+            current_url = current_url + "?page=1";
+        }else{
+            current_url = current_url + "&page=1";
+        }
+    }
+    current_url = current_url.replace("yousuu.com/booklist","yousuu.com/api/booklist")
 
+    var all_page_url = [];
+    for (var i = 1;i<=page;i++){
+        all_page_url.push(current_url.replace(/page=\d+/g,"page="+ i.toString()));
+    }
+    return all_page_url
+
+}
+
+// function show_all() {
+//     // 获得当前书单 页数
+//     let page = document.getElementsByClassName("el-pager").length;
+//     let current_url = window.location.href;
+
+//     all_page_url = get_all_page_url(current_url,page);
+//     console.log(all_page_url)
+
+//     test = all_page_url[0]
+
+//     // get请求
+//     var xhr = new XMLHttpRequest();
+
+//     xhr.open('GET', "https://www.yousuu.com/api/me", true);
+//     xhr.send();
+
+//     xhr.onreadystatechange = function (e) {
+//     if (xhr.readyState == 4 && xhr.status == 200) {
+//         console.log(xhr.responseText);
+//     }
+//     };
+
+// }
+
+
+// 注入 页面
 function create_display() {
 
 
@@ -84,6 +129,7 @@ function create_display() {
     div.setAttribute('id', 'inject_div');
 
     let relative_div = document.getElementsByClassName('BooklistCollectArea');
+    // console.log(relative_div);
     relative_div[0].style.position = 'relative';
     relative_div[0].appendChild(div);
 
@@ -126,14 +172,63 @@ function create_display() {
     button_reload.setAttribute('onclick', 'reload();return false;');
     button_reload.innerHTML = "RELOAD";
 
+    // showAll 按钮
+    // let button_show = document.createElement('a');
+    // button_show.setAttribute('class', 'button_filter');
+    // button_show.setAttribute('href', '#');
+    // button_show.setAttribute('onclick', 'show_all();return false;');
+    // button_show.innerHTML = "SHOWALL";
+
+
+
     // 添加创建的元素
     div.appendChild(select_book_status);
     div.appendChild(select_book_num);
     div.appendChild(button_filter);
     div.appendChild(button_reload);
+    // div.appendChild(button_show);
+
+
+    // let s = document.createElement('div');
+    // // s.setAttribute('class', 'button_filter');
+    // // s.setAttribute('href', '#');
+    // // s.setAttribute('onclick', 'show_all();return false;');
+    // s.innerHTML = '<div class="common-card-layout booklist-book-item">123<div>';
+
+
+
+    // let test_list = document.createElement('div');
+
+    // let list_div = document.getElementsByClassName('result');
+    // // console.log(relative_div);
+    // // relative_div[0].style.position = 'relative';
+    // list_div[0].appendChild(test_list);
+    // test_list.appendChild(s);
+
 
 }
 
+// 到底自动翻页
+window.onscroll = function(){
+    //变量scrollTop是滚动条滚动时，距离顶部的距离
+    var scrollTop = document.documentElement.scrollTop||document.body.scrollTop;
+    //变量windowHeight是可视区的高度
+    var windowHeight = document.documentElement.clientHeight || document.body.clientHeight;
+    //变量scrollHeight是滚动条的总高度
+    var scrollHeight = document.documentElement.scrollHeight||document.body.scrollHeight;
+           //滚动条到底部的条件
+           if(scrollTop+windowHeight == scrollHeight){
+            //到了这个就可以进行业务逻辑加载后台数据了
+            var next_btn = document.getElementsByClassName("btn-next")[0];
 
-create_display();
+            setTimeout(function(){
+                next_btn.click();
+            }, 1500);
+          }   
+    }
+
+
+// create_display();
+setTimeout(create_display, 3000);
+
 
